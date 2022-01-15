e143dfebc122494efb0b94d55cd0381ddf627e70
Mustapha Wolly <wollymustapha@gmail.com>
2022-01-15 12:02:56 -0500

    header file

--- sort.h
+++ sort.h
@@ -26,5 +26,9 @@ void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif
